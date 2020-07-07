fd4e8ad4bad9ecfb565318d93d7abc3fa4808db6
Michael Ciabuca <ciabuca@gmail.com>
2020-07-07 21:19:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
- array = sentence.split(" ")
- array.each_with_index do |value, index|
-  array[index] = array[index].reverse
- end
- puts array
+  reversed = sentence.split(" ").collect do |word|
+    word.reverse
+  end
+  reversed.join(" ")
 end
